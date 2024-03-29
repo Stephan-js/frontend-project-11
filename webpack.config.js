3bf98a60b1b200d2bf72ee1fa524e4140cbdc380
Stephan Kostin <va.lenta0907@gmail.com>
2024-03-30 00:54:04 +0300

    DeBugs

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const config = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public'),
     },
     devServer: {
         static: path.resolve(__dirname, 'dist'),
