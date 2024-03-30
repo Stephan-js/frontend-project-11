7c8430ad3dd823e96e8c12816475f1beed106a2e
Stephan Kostin <va.lenta0907@gmail.com>
2024-03-30 16:55:19 +0300

    Mini update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import _ from 'lodash';
+import { object, string, InferType } from 'yup';
 import './scss/style.scss';
 
-function component() {
-  const element = document.createElement('div');
+const app = () => {
+  const form = document.querySelector('#urls');
+  const status = document.querySelector('.feedback');
+  const state = {
+    addedUrl: [],
+  };
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = e.target.querySelector('input');
+    string().url().nullable().validate(input.value)
+    .then((d) => {
+      if (!state.addedUrl.includes(d)) {
+        status.setAttribute('class', 'feedback m-9 position-absolute small text-success');
+        status.innerHTML = 'RSS aproved!';
+        state.addedUrl.push(d);
+      } else {
+        throw new Error('Sorry, but you cant add one url two times!');
+      }
+    })
+    .catch((e) => {
+      status.setAttribute('class', 'feedback m-9 position-absolute small text-danger');
+      status.innerHTML = e;
+    });
+  });
+};
+app();
