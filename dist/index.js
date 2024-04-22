cfe6f68eb2515892492b6ec0235cc3cf106c8c0f
Stephan Kostin <va.lenta0907@gmail.com>
2024-04-22 20:08:04 +0300

    Rename

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 import './scss/style.scss';
 
-import validator from './functions/validation.js';
-import languageSelector from './functions/language.js';
+import validator from './src/validation.js';
+import languageSelector from './src/language.js';
 
 document.querySelector('main').removeAttribute('style');
 languageSelector();
