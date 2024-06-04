fe20be7070c9ea46e638b4ff07fb66b28cbdbc06
Stephan Kostin <va.lenta0907@gmail.com>
2024-06-04 22:12:44 +0300

    Change lang

--- dist/src/app.js
+++ dist/src/app.js
@@ -468,25 +468,27 @@ const app = () => {
   const avlLng = ['en', 'sp'];
 
   // Select broweser languge if we haven't cookie data and this languge available
-  if (document.cookie === '' && !avlLng.includes(browserL)) {
-    site.setAttribute('lang', browserL);
-    i18next.changeLanguage(browserL);
-    selector.querySelector(`option[value="${browserL}"]`).setAttribute('selected', '');
-    // Select defualt languge - eng
-  } else if (document.cookie === '') {
-    // Added only for tests!
-    site.setAttribute('lang', 'ru');
-    i18next.changeLanguage('ru');
+  //    !!!! For lest deploy !!!!
 
-    //    !!!! For lest deploy !!!!
-    // selector.querySelector('option[value="en"]').setAttribute('selected', '');
-  } else {
-    // Select languge what was in cookie
-    const lng = document.cookie.split('=')[1];
-    site.setAttribute('lang', lng);
-    i18next.changeLanguage(lng);
-    selector.querySelector(`option[value="${lng}"]`).setAttribute('selected', '');
-  }
+  // if (document.cookie === '' && !avlLng.includes(browserL)) {
+  //   site.setAttribute('lang', browserL);
+  //   i18next.changeLanguage(browserL);
+  //   selector.querySelector(`option[value="${browserL}"]`).setAttribute('selected', '');
+  //   // Select defualt languge - eng
+  // } else if (document.cookie === '') {
+  //   site.setAttribute('lang', 'en');
+  //   i18next.changeLanguage('en');
+  //   selector.querySelector('option[value="en"]').setAttribute('selected', '');
+  // } else {
+  //   // Select languge what was in cookie
+  //   const lng = document.cookie.split('=')[1];
+  //   site.setAttribute('lang', lng);
+  //   i18next.changeLanguage(lng);
+  //   selector.querySelector(`option[value="${lng}"]`).setAttribute('selected', '');
+  // }
+
+  site.setAttribute('lang', 'ru');
+  i18next.changeLanguage('ru');
 
   // Last prepare and... Done!
   renderP();
